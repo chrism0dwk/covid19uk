a2e848d4fec19b2fece9997389bee133f3422b17
Chris Jewell <c.jewell@lancaster.ac.uk>
2020-04-01 14:00:13 +0100

    Aligned covid_stochastic.py with the new CovidUK interface.

--- covid_stochastic.py
+++ covid_stochastic.py
@@ -2,12 +2,11 @@ import optparse
 import time
 
 import tensorflow as tf
+import numpy as np
 import matplotlib.pyplot as plt
 import yaml
 
-from covid.model import CovidUKStochastic
-from covid.rdata import *
-from covid.pydata import load_commute_volume
+from covid.model import CovidUKStochastic, load_data
 from covid.util import sanitise_parameter, sanitise_settings, seed_areas
 
 DTYPE = np.float64
@@ -101,8 +100,8 @@ def draw_figs(sim, N):
 
     # TotalUK epidemic curve by age-group
     fig, ax = plt.subplots(1, 2, figsize=[24, 12])
-    plot_by_la(sim, la_names, ax=ax[0])
-    plot_by_age(sim, age_groups, ax=ax[1])
+    plot_by_la(sim, data['la_names'], ax=ax[0])
+    plot_by_age(sim, data['age_groups'], ax=ax[1])
     ax[1].legend()
     plt.xticks(rotation=45, horizontalalignment="right")
     fig.autofmt_xdate()
@@ -111,7 +110,7 @@ def draw_figs(sim, N):
 
     # Plot attack rate
     plt.figure(figsize=[4, 2])
-    plt.plot(age_groups, attack_rate, 'o-')
+    plt.plot(data['age_groups'], attack_rate, 'o-')
     plt.xticks(rotation=90)
     plt.title('Age-specific attack rate')
     plt.savefig('age_attack_rate.pdf')
@@ -132,7 +131,7 @@ def plot_age_attack_rate(ax, sim, N, label):
     Ns = N.reshape([152, 17]).sum(axis=0)
     fs = final_size(sim.numpy())
     attack_rate = fs / Ns
-    ax.plot(age_groups, attack_rate, 'o-', label=label)
+    ax.plot(data['age_groups'], attack_rate, 'o-', label=label)
 
 
 if __name__ == '__main__':
@@ -158,34 +157,20 @@ if __name__ == '__main__':
     param = sanitise_parameter(config['parameter'])
     settings = sanitise_settings(config['settings'])
 
-    M_tt, age_groups = load_age_mixing(config['data']['age_mixing_matrix_term'])
-    M_hh, _ = load_age_mixing(config['data']['age_mixing_matrix_hol'])
+    data = load_data(config['data'], settings, DTYPE)
 
-    C, la_names = load_mobility_matrix(config['data']['mobility_matrix'])
-    np.fill_diagonal(C, 0.)
-
-    W = load_commute_volume(config['data']['commute_volume'], settings['inference_period'])['percent']
-
-    N, n_names = load_population(config['data']['population_size'])
-
-    M_tt = M_tt.astype(DTYPE)
-    M_hh = M_hh.astype(DTYPE)
-    W = W.to_numpy().astype(DTYPE)
-    C = C.astype(DTYPE)
-    N = N.astype(DTYPE)
-
-    model = CovidUKStochastic(M_tt=M_tt,
-                              M_hh=M_hh,
-                              C=C,
-                              N=N,
-                              W=W,
+    model = CovidUKStochastic(M_tt=data['M_tt'],
+                              M_hh=data['M_hh'],
+                              C=data['C'],
+                              N=data['pop']['n'].to_numpy(),
+                              W=data['W'],
                               date_range=settings['inference_period'],
                               holidays=settings['holiday'],
+                              lockdown=settings['lockdown'],
                               time_step=1.)
 
-    seeding = seed_areas(N, n_names)  # Seed 40-44 age group, 30 seeds by popn size
+    seeding = seed_areas(data['pop']['n'].to_numpy(), data['pop']['Area.name.2'])  # Seed 40-44 age group, 30 seeds by popn size
     state_init = model.create_initial_state(init_matrix=seeding)
-
 
     start = time.perf_counter()
     t, sim = model.simulate(param, state_init)
@@ -202,7 +187,7 @@ if __name__ == '__main__':
     fig_attack = plt.figure()
     fig_uk = plt.figure()
 
-    plot_age_attack_rate(fig_attack.gca(), sim, N, "Attack Rate")
+    plot_age_attack_rate(fig_attack.gca(), sim, data['pop']['n'].to_numpy(), "Attack Rate")
     fig_attack.suptitle("Attack Rate")
     plot_infec_curve(fig_uk.gca(), sim.numpy(), "Infections")
     fig_uk.suptitle("UK Infections")
