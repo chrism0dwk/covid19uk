6900b2309085313467510083b40204cfb315cef6
Chris Jewell <c.jewell@lancaster.ac.uk>
2020-10-01 14:29:34 +0100

    Minor changes
    1. removed redundant code from `covid/summary.py`
    2. added to README

--- README.md
+++ README.md
@@ -9,12 +9,22 @@
 * `example_config.yaml` example configuration file containing data paths and MCMC settings
 * `data` a directory containing example data (see below)
 * `environment.yaml` conda description of the required environment.  Create with `conda create -f environment.yaml`
+8 `summary.py` python script to summarise MCMC results into a Geopkg file.
 
 ## Example data files
 * `data/example_cases.csv` a file containing example case data for 43 local authorities in England collected and present of PHE's [website](https://coronavirus.data.gov.uk)
 * `data/example_population.csv` a file containing local authority population data in the UK, taken from ONS prediction for December 2019
 * `data/example_mobility.csv` inter local authority mobility matrix taken from UK Census 2011 commuting data
 * `data/example_traffic_flow` a relative measure of traffic flow taken from mobility metrics from providers such as Google and Facebook.  Data have been smoothed to represent a summary of the original data.
+
+## Example workflow
+```bash
+$ conda env create --prefix=./env -f environment.txt
+$ conda activate ./env
+$ python inference.py
+$ python summary.py
+```
+
 
 ## COVID-19 Lancaster University data statement
 
--- covid/summary.py
+++ covid/summary.py
@@ -23,29 +23,3 @@ def mean_and_ci(arr, q=(0.025, 0.975), axis=0, name=None):
     for i, qq in enumerate(q):
         results[name + str(qq)] = ci[i]
     return results
-
-
-# Reproduction number calculation
-def calc_R_it(theta, xi, events, init_state, covar_data):
-    """Calculates effective reproduction number for batches of metapopulations
-    :param theta: a tensor of batched theta parameters [B] + theta.shape
-    :param xi: a tensor of batched xi parameters [B] + xi.shape
-    :param events: a [B, M, T, X] batched events tensor
-    :param init_state: the initial state of the epidemic at earliest inference date
-    :param covar_data: the covariate data
-    :return a batched vector of R_it estimates
-    """
-
-    def r_fn(args):
-        theta_, xi_, events_ = args
-        t = events_.shape[-2] - 1
-        state = compute_state(init_state, events_, model_spec.STOICHIOMETRY)
-        state = tf.gather(state, t - 1, axis=-2)  # State on final inference day
-
-        par = dict(beta1=theta[0], beta2=theta[1], gamma=theta[2], nu=0.5, xi=xi)
-
-        ngm_fn = model_spec.next_generation_matrix_fn(covar_data, par)
-        ngm = ngm_fn(t, state)
-        return ngm
-
-    return tf.vectorized_map(r_fn, elems=(theta, xi, events))
